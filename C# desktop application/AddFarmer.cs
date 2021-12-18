0f52840498ef31535b0e1c965cfd51767206a89a
akashweerathunga <gadse191f-013@student.nibm.lk>
2021-12-18 16:13:42 +0530

    add comment

--- C# desktop application/AddFarmer.cs
+++ C# desktop application/AddFarmer.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 using MySql.Data.MySqlClient;
 namespace ProjectZ
-{
+{  //comment
     public partial class AddFarmer :  Form
     {
         public AddFarmer()
